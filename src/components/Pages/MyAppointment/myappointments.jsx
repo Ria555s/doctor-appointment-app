22ade0cc281ac069c33091be4fc5e4e5a02daeda
Ria Sadhu <riasadhu4@gmail.com>
2025-04-27 14:15:33 +0530

    Update myappointments.jsx
    4th

--- src/components/Pages/MyAppointment/myappointments.jsx
+++ src/components/Pages/MyAppointment/myappointments.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './MyAppointment.css';
+import './myappointment.css';
 import medicalImage from './images/medical.jpg';
 import { Eye } from 'lucide-react';
 
